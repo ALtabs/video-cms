756d0363a381574fab7a32d79e676f7d9aa5ee69
Owl-Mah-Soul <almahsol.bposeats@gmail.com>
2024-10-18 19:00:17 +0800

    update changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from videos.views import LoginView, VideoViewSet,
+from videos.views import LoginView, VideoViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
