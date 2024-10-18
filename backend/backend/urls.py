89d34b9adaca07b740cfdba32c2c9c6d6d85ee4e
Owl-Mah-Soul <almahsol.bposeats@gmail.com>
2024-10-18 18:10:59 +0800

    push new url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from videos.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('videos.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('videos/', include('videos.urls')),
 ]
--- backend/videos/urls.py
+++ backend/videos/urls.py
@@ -10,7 +10,6 @@ router.register(r'videos', VideoViewSet)
 # Defining urlpatterns
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),  # This includes all routes defined by the router
