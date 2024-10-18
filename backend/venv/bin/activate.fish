7cfcc1140183778eb00b3dd93339a4dcff2b2614
Owl-Mah-Soul <almahsol.bposeats@gmail.com>
2024-10-18 10:06:40 +0800

    push new update

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/cf/video-cms/backend/venv"
+set -gx VIRTUAL_ENV "/home/cf/video-cms/backend/new_venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(venv) "
-            printf "%s%s" "(venv) " (set_color normal)
+        if test -n "(new_venv) "
+            printf "%s%s" "(new_venv) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
