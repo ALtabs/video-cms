df3225d4026d0d14adc48d64c25ded659c7c5d3a
Owl-Mah-Soul <almahsol.bposeats@gmail.com>
2024-10-18 19:32:37 +0800

    turn on debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zv9y$)r9l7tum+@mbas0am(!n0tppjg!%eh+irtogh*akv9%e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'video-cms-1.onrender.com',
